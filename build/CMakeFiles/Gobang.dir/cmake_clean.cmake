34b57a1ee6e8cb561c109b4ee41af37f0e8f34b3
suiqingying <2252393382@qq.com>
2024-09-19 19:15:18 +0800

    zc

---
+++ build/CMakeFiles/Gobang.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gobang.dir/main.c.o"
+  "CMakeFiles/Gobang.dir/main.c.o.d"
+  "Gobang"
+  "Gobang.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Gobang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Gobang!\n");
+    return 0;
+}
