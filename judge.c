eab2c3698dd39e60099524c60541365ab59838eb
suiqingying <2252393382@qq.com>
2024-11-11 00:04:39 +0800

    Second Edition

--- judge.c
+++ judge.c
@@ -1,7 +1,9 @@
 #include "judge.h"
 
-bool judge(int x, int y, int player, int game_mode) {
-    if (player == 2) return !isWin(x, y, player);
+bool judge(int x, int y, int player, int game_mode)
+{
+    if (player == 2)
+        return !isWin(x, y, player);
     int statue = BanMove(x, y);
     if (statue) {
         if (statue == 1)
@@ -10,9 +12,9 @@ bool judge(int x, int y, int player, int game_mode) {
             printf("Double four ban!\n");
         else if (statue == 3)
             printf("Too long ban!\n");
-        if(game_mode == 1)
+        if (game_mode == 1)
             printf("Player %d wins!\n", 3 - player);
-        else 
+        else
             printf("You Lost!RUBBISH!\n");
         return false;
     }
@@ -74,226 +76,157 @@ bool isWin(int x, int y, int player)
 
 int BanMove(int x, int y)
 {
-    int newX,newY;    
-    int adjoin_black[8]; 
-    int adjoin_empty[8]; 
-    int gap_black[8];     
-    int gap_empty[8];     
-    int gap_gap_black[8];  
-    for (int i = 0; i < 8; i++)
-    {
+    int newX, newY;
+    int adjoin_black[8];
+    int adjoin_empty[8];
+    int gap_black[8];
+    int gap_empty[8];
+    int gap_gap_black[8];
+    for (int i = 0; i < 8; i++) {
         adjoin_black[i] = adjoin_empty[i] = gap_black[i] = gap_empty[i] = gap_gap_black[i] = 0;
     }
-    for (int i = 0; i < 4; i++)
-    {
+    for (int i = 0; i < 4; i++) {
         newX = x + dx[i], newY = y + dy[i];
-        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX += dx[i], newY += dy[i], adjoin_black[i]++);
-        for (; isInBounds(newX, newY) && board[newX][newY] == 0; newX += dx[i], newY += dy[i], adjoin_empty[i]++);
-        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX += dx[i], newY += dy[i], gap_black[i]++);
-        for (; isInBounds(newX, newY) && board[newX][newY] == 0; newX += dx[i], newY += dy[i], gap_empty[i]++);
-        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX += dx[i], newY += dy[i], gap_gap_black[i]++);
+        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX += dx[i], newY += dy[i], adjoin_black[i]++)
+            ;
+        for (; isInBounds(newX, newY) && board[newX][newY] == 0; newX += dx[i], newY += dy[i], adjoin_empty[i]++)
+            ;
+        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX += dx[i], newY += dy[i], gap_black[i]++)
+            ;
+        for (; isInBounds(newX, newY) && board[newX][newY] == 0; newX += dx[i], newY += dy[i], gap_empty[i]++)
+            ;
+        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX += dx[i], newY += dy[i], gap_gap_black[i]++)
+            ;
     }
-    for (int i = 0; i < 4; i++)
-    {
+    for (int i = 0; i < 4; i++) {
         newX = x - dx[i], newY = y - dy[i];
-        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX -= dx[i], newY -= dy[i], adjoin_black[i + 4]++);
-        for (; isInBounds(newX, newY) && board[newX][newY] == 0; newX -= dx[i], newY -= dy[i], adjoin_empty[i + 4]++);
-        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX -= dx[i], newY -= dy[i], gap_black[i + 4]++);
-        for (; isInBounds(newX, newY) && board[newX][newY] == 0; newX -= dx[i], newY -= dy[i], gap_empty[i + 4]++);
-        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX -= dx[i], newY -= dy[i], gap_gap_black[i + 4]++);
+        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX -= dx[i], newY -= dy[i], adjoin_black[i + 4]++)
+            ;
+        for (; isInBounds(newX, newY) && board[newX][newY] == 0; newX -= dx[i], newY -= dy[i], adjoin_empty[i + 4]++)
+            ;
+        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX -= dx[i], newY -= dy[i], gap_black[i + 4]++)
+            ;
+        for (; isInBounds(newX, newY) && board[newX][newY] == 0; newX -= dx[i], newY -= dy[i], gap_empty[i + 4]++)
+            ;
+        for (; isInBounds(newX, newY) && board[newX][newY] == 1; newX -= dx[i], newY -= dy[i], gap_gap_black[i + 4]++)
+            ;
     }
-    int con_four = 0;       //四连(包含活四、冲四)数量
-    int active_three = 0;   //活三数量
-    int whe_four;           //记录四连两端的落子点状态
-    int whe_three;          //记录活三两端的落子点状态
-    
-    for (int i = 0; i < 4; i++)
-    {        
-        if (adjoin_black[i] + adjoin_black[i + 4] == 4)
-        {
+    int Num_4 = 0;
+    int Num_3 = 0;
+    int whe_four;
+    int whe_three;
+
+    for (int i = 0; i < 4; i++) {
+        if (adjoin_black[i] + adjoin_black[i + 4] == 4) {
             return 0;
         }
-        if (adjoin_black[i] + adjoin_black[i + 4] >= 5)
-        {
+        if (adjoin_black[i] + adjoin_black[i + 4] >= 5) {
             return 3;
         }
     }
 
-    //判活四或冲四
-    for (int i = 0; i < 4; i++)
-    {
-        if (adjoin_black[i] + adjoin_black[i + 4] == 3)
-        {
-            whe_four = 0;//初始化·
-            if (adjoin_empty[i] > 0)//四连棋形：1111_
-            {
-                if(!Reban(x, y, i, adjoin_black[i] + 1, 1))
-                {
+    for (int i = 0; i < 4; i++) {
+        whe_four = 0;
+        whe_three = 0;
+        if (adjoin_black[i] + adjoin_black[i + 4] == 3) {
+            if (adjoin_empty[i] > 0) {
+                if (!Reban(x, y, i, adjoin_black[i] + 1, 1))
                     whe_four = 1;
-                }
-            }
-            else if (adjoin_empty[i + 4] > 0)//活四棋形：_1111
-            {
-                if(!Reban(x, y, i, adjoin_black[i + 4] + 1, -1))
-                {
+            } // 1111_
+            if (adjoin_empty[i + 4] > 0) {
+                if (!Reban(x, y, i, adjoin_black[i] + 1, -1))
                     whe_four = 1;
-                }
-            }
+            } // _1111
             if (whe_four)
-            {
-                con_four++;
-            }
+                Num_4++;
         }
-    }
 
-    //落子形成三连
-    for (int i = 0; i < 4; i++)
-    {
-        if (adjoin_black[i] + adjoin_black[i + 4] == 2)
-        {
-            //检查是否为间隔的冲四
-            if (adjoin_empty[i] == 1 && gap_black[i] == 1)//冲四棋形：111_1
-            {
-                if(!Reban(x, y, i, adjoin_black[i] + 1, 1))
-                {
-                    con_four++;
-                }
-            }
-            if (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 1)//冲四棋形：1_111
-            {
-                if(!Reban(x, y, i, adjoin_black[i + 4] + 1, -1))
-                {
-                    con_four++;
-                }
-            }
+        if (adjoin_black[i] + adjoin_black[i + 4] == 2) {
+            if (adjoin_empty[i] == 1 && gap_black[i] == 1) {
+                if (!Reban(x, y, i, adjoin_black[i] + 1, 1)) whe_four = 1;
+            } // 111_1
+            if (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 1) {
+                if (!Reban(x, y, i, adjoin_black[i + 4] + 1, -1))
+                    whe_four = 1;
+            } // 1_111
+            if (whe_four)
+                Num_4++;
 
-            //检查是否为活三
-            whe_three = 0;//初始化
-            if ((adjoin_empty[i] > 2 || (adjoin_empty[i] == 2 && gap_black[i] == 0)) && 
-            (adjoin_empty[i + 4] > 1 || (adjoin_empty[i + 4] = 1 && gap_black[i + 4] == 0)))//活三棋形：?_111__?
-            {
-                //活三右端的落子点状态，排除非活四情况，故能落子则必为活三
-                if(!Reban(x, y, i, adjoin_black[i] + 1, 1))
-                {
+            if ((adjoin_empty[i] > 2 || (adjoin_empty[i] == 2 && gap_black[i] == 0)) && (adjoin_empty[i + 4] > 1 || (adjoin_empty[i + 4] = 1 && gap_black[i + 4] == 0))) {
+                if (!Reban(x, y, i, adjoin_black[i] + 1, 1))
                     whe_three = 1;
-                }
-            }
-            if ((adjoin_empty[i + 4] > 2 || (adjoin_empty[i + 4] == 2 && gap_black[i + 4] == 0)) &&
-            (adjoin_empty[i] > 1 || (adjoin_empty[i] == 1 && gap_black[i] == 0)))//活三棋形：?__111_?
-            {
-                //活三左端的落子点状态，排除非活四情况，故能落子则必为活三
-                if(!Reban(x, y, i, adjoin_black[i + 4] + 1, -1))
-                {
+            } //?_111__?
+            if ((adjoin_empty[i + 4] > 2 || (adjoin_empty[i + 4] == 2 && gap_black[i + 4] == 0)) && (adjoin_empty[i] > 1 || (adjoin_empty[i] == 1 && gap_black[i] == 0))) {
+                if (!Reban(x, y, i, adjoin_black[i + 4] + 1, -1))
                     whe_three = 1;
-                }
-            }
-            //根据两端的落子点状态判断是否为活三
+            } //?__111_?
             if (whe_three)
+                Num_3++;
+        }
+
+        if (adjoin_black[i] + adjoin_black[i + 4] == 1) {
+            if (adjoin_empty[i] == 1 && gap_black[i] == 2) 
             {
-                active_three++;
-            }
+               if (!Reban(x, y, i, adjoin_black[i] + 1, 1))
+                    Num_4++;
+            } //?11_11?
+
+            if (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 2) 
+            {
+                if (!Reban(x, y, i, adjoin_black[i + 4] + 1, -1)) 
+                    Num_4++;
+            } //?11_11?
+
+            if (adjoin_empty[i] = 1 && gap_black[i] == 1 && (gap_empty[i] > 1 || (gap_empty[i] == 1 && gap_gap_black[i] == 0)) && (adjoin_empty[i + 4] > 1 || (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 0))) //?_11_1_?
+            {
+                if (!Reban(x, y, i, adjoin_black[i] + 1, 1)) 
+                    Num_3++;
+            } //?_11_1_?
+            if (adjoin_empty[i + 4] = 1 && gap_black[i + 4] == 1 && (gap_empty[i + 4] > 1 || (gap_empty[i + 4] == 1 && gap_gap_black[i + 4] == 0)) && (adjoin_empty[i] > 1 || (adjoin_empty[i] == 1 && gap_black[i] == 0))) //?_1_11_?
+            {
+                if (!Reban(x, y, i, adjoin_black[i + 4] + 1, -1)) 
+                    Num_3++;
+            } //?_1_11_?
+        }
+
+        if (adjoin_black[i] + adjoin_black[i + 4] == 0) {
+            if (adjoin_empty[i] == 1 && gap_black[i] == 3) 
+            {
+                if (!Reban(x, y, i, adjoin_black[i] + 1, 1)) 
+                    Num_4++;
+            } //?1_111?
+            if (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 3) 
+            {
+                if (!Reban(x, y, i, adjoin_black[i + 4] + 1, -1)) {
+                    Num_4++;
+                }
+            } //?111_1?
+
+            if (adjoin_empty[i] == 1 && gap_black[i] == 2 && (gap_empty[i] > 1 || (gap_empty[i] == 1 && gap_gap_black[i] == 0)) && (adjoin_empty[i + 4] > 1 || (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 0))) 
+            {
+                if (!Reban(x, y, i, adjoin_black[i] + 1, 1)) {
+                    Num_3++;
+                }
+            } //?_1_11_?
+            if (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 2 && (gap_empty[i + 4] > 1 || (gap_empty[i + 4] == 1 && gap_gap_black[i + 4] == 0)) && (adjoin_empty[i] > 1 || (adjoin_empty[i] == 1 && gap_black[i] == 0))) 
+            {
+                if (!Reban(x, y, i, adjoin_black[i + 4] + 1, -1)) {
+                    Num_3++;
+                }
+            } //?_11_1_?
         }
     }
 
-    //落子形成二连
-    for (int i = 0; i < 4; i++)
-    {
-        if (adjoin_black[i] + adjoin_black[i + 4] == 1)
-        {
-            //检查是否为间隔的冲四
-            if (adjoin_empty[i] == 1 && gap_black[i] == 2)//冲四棋形：?11_11?，对“右”方向的判断
-            {
-                if (!BanMove(x + dx[i] * (adjoin_black[i] + 1), y + dy[i] * (adjoin_black[i] + 1)))
-                {
-                    con_four++;
-                }
-            }
-            if (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 2)//冲四棋形：?11_11?，对“左”方向的判断
-            {
-                if (!BanMove(x - dx[i] * (adjoin_black[i + 4] + 1), y - dy[i] * (adjoin_black[i] + 1)))
-                {
-                    con_four++;
-                }
-            }
-
-            //检查是否为活三
-            if ( adjoin_empty[i] = 1 && gap_black[i] == 1 && (gap_empty[i] > 1 || (gap_empty[i] == 1 && gap_gap_black[i] == 0)) &&
-            (adjoin_empty[i + 4] > 1 || (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 0)))//活三棋形：?_11_1_?,对“右”方向的判断
-            {
-                //二连右端的落子点状态，已经人为排除非活四情况，故能落子则必为活三
-                if(!Reban(x, y, i, adjoin_black[i] + 1, 1))
-                {
-                    active_three++;
-                }
-            }
-            if (adjoin_empty[i + 4] = 1 && gap_black[i + 4] == 1 && (gap_empty[i + 4] > 1 || (gap_empty[i + 4] == 1 && gap_gap_black[i + 4] == 0)) &&
-            (adjoin_empty[i] > 1 || (adjoin_empty[i] == 1 && gap_black[i] == 0)))//活三棋形：?_1_11_?,对“左”方向的判断
-            {
-                //二连左端的落子点状态，已经人为排除非活四情况，故能落子则必为活三
-                if(!Reban(x, y, i, adjoin_black[i + 4] + 1, -1))
-                {
-                    active_three++;
-                }
-            }
-        }
-    }
-
-    //落子形成单连
-    for (int i = 0; i < 4; i++)
-    {
-        if (adjoin_black[i] + adjoin_black[i + 4] == 0)
-        {
-            //检查是否为间隔的冲四
-            if (adjoin_empty[i] == 1 && gap_black[i] == 3)//冲四棋形：?1_111?，对“右”方向的判断
-            {
-                if(!Reban(x, y, i, adjoin_black[i] + 1, 1))
-                {
-                    con_four++;
-                }
-            }
-            if (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 3)//冲四棋形：?1_111?，对“左”方向的判断
-            {
-                if(!Reban(x, y, i, adjoin_black[i + 4] + 1, -1))
-                {
-                    con_four++;
-                }
-            }
-
-            //检查是否为活三
-            if (adjoin_empty[i] == 1 && gap_black[i] == 2 && (gap_empty[i] > 1 || (gap_empty[i] == 1 && gap_gap_black[i] == 0)) &&
-            (adjoin_empty[i + 4] > 1 || (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 0)))//活三棋形：?_1_11_?,对“右”方向的判断
-            {
-                //单连右端的落子点状态，已经人为排除非活四情况，故能落子则必为活三
-                if(!Reban(x, y, i, adjoin_black[i] + 1, 1))
-                {
-                    active_three++;
-                }
-            }
-            if (adjoin_empty[i + 4] == 1 && gap_black[i + 4] == 2 && (gap_empty[i + 4] > 1 || (gap_empty[i + 4] == 1 && gap_gap_black[i + 4] == 0)) &&
-            (adjoin_empty[i] > 1 || (adjoin_empty[i] == 1 && gap_black[i] == 0)))//活三棋形：?_11_1_?,对“左”方向的判断
-            {
-                //单连左端的落子点状态，已经人为排除非活四情况，故能落子则必为活三
-                if(!Reban(x, y, i, adjoin_black[i + 4] + 1, -1))
-                {
-                    active_three++;
-                }
-            }
-        }
-    }
-    //根据活三、活四数量判断是否为禁手
-    if (active_three >= 2)//活三数量大于等于2
-    {
+    if (Num_3 >= 2)
         return 1;
-    }
-    if (con_four >= 2)//活四数量大于等于2
-    {
+    if (Num_4 >= 2)
         return 2;
-    }
 
-    return 0;//无禁手，返回值0
+    return 0; // 无禁手
 }
 
-int Reban(int x, int y, int direction, int num, int fl) {
+int Reban(int x, int y, int direction, int num, int fl)
+{
     board[x][y] = 1;
     int res = BanMove(x + fl * dx[direction] * num, y + fl * dy[direction] * num);
     board[x][y] = 0;
